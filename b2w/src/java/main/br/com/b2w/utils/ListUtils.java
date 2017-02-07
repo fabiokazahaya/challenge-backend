1d1f1ce2166d6a41ce510247009ab570491d8a27
fkazahaya <fkazahaya@kazahaya-localdomain>
2017-02-07 19:47:54 -0200

    Validation.

--- b2w/src/java/main/br/com/b2w/utils/ListUtils.java
+++ b2w/src/java/main/br/com/b2w/utils/ListUtils.java
@@ -11,20 +11,20 @@ import br.com.b2w.exception.GenericException;
 import br.com.b2w.model.Item;
 
 public class ListUtils {
-	
+
 	static final Logger LOG = LoggerFactory.getLogger(ListUtils.class);
-	
-	public static List<Item> getListOfItem(ZonedDateTime beginZdt, ZonedDateTime endZdt, List<Item> listOfZonedDateTime) {
-		try {
-			return listOfZonedDateTime.parallelStream()
-					.filter(s -> DateUtils.convertStringToZonedDateTime(s.getDate())
-					.isBefore(endZdt) && DateUtils.convertStringToZonedDateTime(s.getDate())
-					.isAfter(beginZdt))
-					.collect(Collectors.toList());
-		} catch (RuntimeException e) {
-			LOG.error("Erro ao recuperar a lista de Items.", e);
-			throw new GenericException("Erro ao recuperar a lista de Items.", e);
+
+	public static List<Item> getListOfItem(ZonedDateTime beginZdt, ZonedDateTime endZdt,
+			List<Item> listOfZonedDateTime) {
+		List<Item> listOfItemsRange = listOfZonedDateTime.parallelStream()
+				.filter(s -> DateUtils.convertStringToZonedDateTime(s.getDate()).isBefore(endZdt)
+						&& DateUtils.convertStringToZonedDateTime(s.getDate()).isAfter(beginZdt))
+				.collect(Collectors.toList());
+		if (listOfItemsRange.isEmpty()) {
+			LOG.error("Lista vazia, tente novamente");
+			throw new GenericException("Lista vazia, tente novamente");
 		}
+		return listOfItemsRange;
 	}
 }
 
