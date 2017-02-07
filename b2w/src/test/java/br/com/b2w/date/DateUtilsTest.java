f8c84149085f8103675a05d280a2807f31a4fefc
Kazahaya, Fabio <Kazahaya, Fabio@VITORIA.raad.brazil>
2017-02-07 15:56:21 -0200

    Junit.

---
+++ b2w/src/test/java/br/com/b2w/date/DateUtilsTest.java
@@ -0,0 +1,16 @@
+package br.com.b2w.date;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class DateUtilsTest {
+	
+	@Test
+	public void test(){
+		
+		Assert.assertTrue(true);
+		
+	}
+
+}
